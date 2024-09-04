b8ad2a9a198bd143d0befee12c0ce0b3965ce78d
MartinEllebyBerg <160470389+MartinEllebyBerg@users.noreply.github.com>
2024-09-04 14:45:08 +0200

    Færdig

---
+++ src/main/java/com/example/emhaandboldmini/EmHaandboldMiniApplication.java
@@ -0,0 +1,13 @@
+package com.example.emhaandboldmini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmHaandboldMiniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmHaandboldMiniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/emhaandboldmini/EmHaandboldMiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emhaandboldmini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmHaandboldMiniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
